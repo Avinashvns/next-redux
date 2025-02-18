967b314a4703314a7e3b57f2b6b42b20b558963c
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-18 15:23:24 +0530

    Add Counter ( Display ) in Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Counter from "@/component/Counter";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <h1>Home Page</h1>
+      <Counter />
     </>
   );
 }
